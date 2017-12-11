f8bfe6368939047a112ddff0423ecd6c7c0e855e
yutashi <katayama@xshell.io>
2017-12-11 04:17:34 +0000

    Initial commit

---
+++ camera/camera.py
@@ -0,0 +1,25 @@
+from imutils.video.pivideostream import PiVideoStream
+import time
+import datetime
+import numpy as np
+import cv2
+
+
+class VideoCamera(object):
+    def __init__(self, flip = False):
+        self.vs = PiVideoStream(resolution=(800, 608)).start()
+        self.flip = flip
+        time.sleep(2.0)
+
+    def __del__(self):
+        self.vs.stop()
+
+    def flip_if_needed(self, frame):
+        if self.flip:
+            return np.flip(frame, 0)
+        return frame
+
+    def get_frame(self):
+        frame = self.flip_if_needed(self.vs.read())
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
