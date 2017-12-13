c26b00413ba81f94c306123e18ed6a7408d9172e
yutashi <katayama@xshell.io>
2017-12-13 08:16:03 +0000

    Add new processor to detect motion

---
+++ camera/processor/motion_detector.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+from imutils.video.pivideostream import PiVideoStream
+import imutils
+import time
+import numpy as np
+import cv2
+
+
+class MotionDetector(object):
+    def __init__(self, flip = True):
+        self.vs = PiVideoStream(resolution=(800, 608)).start()
+        self.flip = flip
+        time.sleep(2.0)
+
+        self.avg = None
+        
+    def __del__(self):
+        self.vs.stop()
+
+    def flip_if_needed(self, frame):
+        if self.flip:
+            return np.flip(frame, 0)
+        return frame
+
+    def get_frame(self):
+        frame = self.flip_if_needed(self.vs.read())
+        frame = self.process_image(frame)
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+
+    def process_image(self, frame):
+        frame = imutils.resize(frame, width=min(500, frame.shape[1]))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        if self.avg is None:
+            print('Starting background model...')
+            self.avg = gray.copy().astype('float')
+            return frame
+
+        cv2.accumulateWeighted(gray, self.avg, 0.5)
+        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(self.avg))
+        thresh = cv2.threshold(frameDelta, 5, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+        for c in cnts:
+            if cv2.contourArea(c) < 5000:
+                continue
+
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        return frame
