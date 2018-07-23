80e41420185f6c5a213e708b92fa9d134da616a8
yuta <katayama@xshell.io>
2018-07-23 16:26:52 +0900

    Add basic function to detect faces

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -12,6 +12,7 @@ class FaceDetector(object):
         time.sleep(2.0)
 
         # opencvの顔分類器(CascadeClassifier)をインスタンス化する
+        self.face_cascade = cv2.CascadeClassifier('camera/processor/model/haarcascades/haarcascade_frontalface_default.xml')
 
     def __del__(self):
         self.vs.stop()
@@ -29,12 +30,16 @@ class FaceDetector(object):
 
     def process_image(self, frame):
         # opencvでframe(カラー画像)をグレースケールに変換
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # 上記でグレースケールに変換したものをインスタンス化した顔分類器の
         # detectMultiScaleメソッドで処理し、認識した顔の座標情報を取得する
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 3)
         
         # 取得した座標情報を元に、cv2.rectangleを使ってframe上に
         # 顔の位置を描画する
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
         # frameを戻り値として返す
         return frame
